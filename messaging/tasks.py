4a6d65b3f0e3fdb617cc1519b7c4fb289b1a6e3b
Mathieu Fuchs <mathieufuchs13@gmail.com>
2015-09-29 16:31:26 +0200

    init labb3

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -13,3 +13,25 @@ def gen_prime(x):
             for j in xrange(i*i, x+1, i):
                 multiples.append(j)
     return results
+@app.task
+def parseTweets():
+	import json
+	pronoms=["han", "hon", "den", "det", "denna", "denne", "hen"] 
+	pronoms_count= [0,0,0,0,0,0,0]
+	objects = open('tweetsTemp.txt', 'w')
+	objects.write(obj[1])
+	objects.close()
+	objects = open('tweetsTemp.txt', 'r')
+	for line in objects:
+	    try:
+	        tmp = json.loads(line)["text"]
+	        retweet = json.loads(line)["retweeted"]
+	        if retweet == false:
+	        	for i in range(len(pronoms)):
+	            	if(pronoms[i] in tmp):
+	                	pronoms_count[i] += 1
+	    except:
+	        pass
+	for i in range(len(pronoms)):
+	    print "Antal %s: %i" %(pronoms[i], pronoms_count[i])
+	objects.close()
