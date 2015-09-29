6ed6c83601d684b269b6bf66b53500c3e75a2482
Mathieu Fuchs <mathieufuchs13@gmail.com>
2015-09-29 17:27:48 +0200

    added

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -3,35 +3,41 @@ app = Celery('tasks', backend='amqp', broker='amqp://')
 @app.task(ignore_result=True)
 def print_hello():
     print 'hello there'
-@app.task
-def gen_prime(x):
-    multiples = []
-    results = []
-    for i in xrange(2, x+1):
-        if i not in multiples:
-            results.append(i)
-            for j in xrange(i*i, x+1, i):
-                multiples.append(j)
-    return results
+
+
 @app.task
 def parseTweets():
+	import os
+	import swiftclient.client
 	import json
+	config = {'user':os.environ['OS_USERNAME'], 
+          'key':os.environ['OS_PASSWORD'],
+          'tenant_name':os.environ['OS_TENANT_NAME'],
+          'authurl':os.environ['OS_AUTH_URL']}
+
+	conn = swiftclient.client.Connection(auth_version=2, **config)
+
 	pronoms=["han", "hon", "den", "det", "denna", "denne", "hen"] 
 	pronoms_count= [0,0,0,0,0,0,0]
-	objects = open('tweetsTemp.txt', 'w')
-	objects.write(obj[1])
-	objects.close()
-	objects = open('tweetsTemp.txt', 'r')
-	for line in objects:
-	    try:
-	        tmp = json.loads(line)["text"]
-	        retweet = json.loads(line)["retweeted"]
-	        if retweet == false:
-	        	for i in range(len(pronoms)):
-	            	if(pronoms[i] in tmp):
-	                	pronoms_count[i] += 1
-	    except:
-	        pass
+
+	tweets = conn.get_container("tweets")[1]
+	for t in tweets:
+    	print t["name"]
+    	obj = conn.get_object("tweets", t["name"])
+		objects = open('tweetsTemp.txt', 'w')
+		objects.write(obj[1])
+		objects.close()
+		objects = open('tweetsTemp.txt', 'r')
+		for line in objects:
+		    try:
+		        tmp = json.loads(line)["text"]
+		        retweet = json.loads(line)["retweeted"]
+		        if retweet == false:
+		        	for i in range(len(pronoms)):
+		            	if(pronoms[i] in tmp):
+		                	pronoms_count[i] += 1
+		    except:
+		        pass
+		objects.close()
 	for i in range(len(pronoms)):
 	    print "Antal %s: %i" %(pronoms[i], pronoms_count[i])
-	objects.close()
