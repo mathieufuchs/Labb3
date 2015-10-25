f2654515711e7c4aec61e4c96ff635f5ee9794b3
Mathieu Fuchs <mathieufuchs13@gmail.com>
2015-10-25 11:30:05 +0100

    changed retweet_count to retweted_status

--- messaging/app.py
+++ messaging/app.py
@@ -28,23 +28,12 @@ def cow_say():
 	for t in tweetsObject:
 		tweets.append(t)
 
-	A = tweets[:4]
-	B = tweets[4:8]
-	C = tweets[8:12]
-	D = tweets[12:16]
-	E = tweets[16:]
-
-	job = group(parseTweets.s(A), 
-		parseTweets.s(B), 
-		parseTweets.s(C),
-		parseTweets.s(D),
-		parseTweets.s(E))
-
+	job = group(parseTweets.s([i]) for i in tweets)
 	tweetTask = job.apply_async()
 	print "Celery is working..."
 	counter = 0
 	while (tweetTask.ready() == False):
-		print "... %i s" %(counter)
+		#print "... %i s" %(counter)
 		counter += 5
 		time.sleep(5)
 	print "The task is done!"
@@ -54,9 +43,9 @@ def cow_say():
 	c = Counter()
 	for d in toReturn:
 		c.update(d)
-
-	return jsonify(dict(c)), 200
+	
+	display = dict(c)
+	return jsonify(display), 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',debug=True)
-
--- messaging/tasks.py
+++ messaging/tasks.py
@@ -2,40 +2,39 @@ from celery import Celery
 import os
 import swiftclient.client
 import json
+import time
 import urllib2
 app = Celery('tasks', backend='amqp', broker='amqp://ma:fu@130.238.29.171:5672/mafu')
 
 @app.task()
-def parseTweets(tweetName):
+def parseTweets(t):
 
 	pronoms={"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
 	n = 0 
-
-	#tweets = conn.get_container("tweets")[1]
-	#tweets = tweets[0:1]
-	#tweets = [{"name":"tweets_19.txt"}]
-	for t in tweetName:
-		print t
-		req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
-		response = urllib2.urlopen(req)
-		obj = response.read()
-		objects = open(t, 'w')
-		objects.write(obj)
-		objects.close()
-		objects = open(t, 'r')
-		for line in objects:
-			try:
-				jL = json.loads(line)
-				tmp1 = jL["text"].lower()
-				tmp = tmp1.split()
-				retweet = jL["retweet_count"]
-				if retweet == 0:
-					n += 1
-					for i in pronoms.keys():
-						if(i in tmp):
-							pronoms[i] += 1
-			except:
+	startTime = time.time()
+	req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
+	response = urllib2.urlopen(req)
+	obj = response.read()
+	objects = open(t, 'w')
+	objects.write(obj)
+	objects.close()
+	objects = open(t, 'r')
+	for line in objects:
+		try:
+			jL = json.loads(line)
+			tmp1 = jL["text"].lower()
+			tmp = tmp1.split()
+			retweet = jL["retweet_count"]
+			if jL["retweeted_status"]:
 				pass
-		objects.close()
-	pronoms.update({"num_of_tweets": n})
+			else:
+				n += 1
+				for i in pronoms.keys():
+					if(i in tmp):
+						pronoms[i] += 1
+		except:
+			pass
+	objects.close()
+	time_elapsed = (time.time() - startTime)
+	pronoms.update({"num_of_tweets": n, "time_elapsed":time_elapsed})
 	return pronoms
